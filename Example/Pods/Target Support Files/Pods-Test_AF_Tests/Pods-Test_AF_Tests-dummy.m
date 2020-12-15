236cc66ecbb8e40fa501fce47a24006d3f6906b8
王湘衡 <wangxiangheng@wangxianghengdeMacBook-Pro.local>
2020-12-15 22:39:14 +0800

    intatial commit of testLibrary
    Signed-off-by: 王湘衡 <wangxiangheng@wangxianghengdeMacBook-Pro.local>

---
+++ Example/Pods/Target Support Files/Pods-Test_AF_Tests/Pods-Test_AF_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_AF_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Test_AF_Tests
+@end
---
+++ Example/Pods/Target Support Files/Test_AF/Test_AF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Test_AF : NSObject
+@end
+@implementation PodsDummy_Test_AF
+@end
